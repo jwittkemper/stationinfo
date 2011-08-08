81317f8f39ade825e4e05b962cdad15536ebfd04
wache14@gmail.com <wache14@gmail.com@792a01fb-7bbf-7be1-e469-c52ca8047c5e>
2011-08-08 16:46:08 +0000

    Zwischenstand zur nächsten Anpassung:

--- src/biz/wittkemper/utils/Utils.java
+++ src/biz/wittkemper/utils/Utils.java
@@ -5,36 +5,58 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
-
-
 
 public class Utils {
 	final static String filename = "properties.xml";
-	final static String database ="DATABASE";
-	final static String master ="MASTER";
-	public enum STATIONPROP {DATABASE, MASTER };
+	final static String database = "DATABASE";
+	final static String master = "MASTER";
+
+	public enum STATIONPROP {
+		DATABASE, MASTER
+	};
+
 	
-	public static boolean PropertieFileExist(){
-		
+	private static Map<STATIONPROP, String> list = new HashMap<STATIONPROP, String>();
+
+	public static boolean PropertieFileExist() {
+
 		File file = new File(filename);
 		return file.exists();
 	}
+
+	public static boolean checkMaster() {
+		String lmaster = getPropertie(STATIONPROP.MASTER);
+		if (lmaster != null && lmaster.equalsIgnoreCase("false")) {
+			return false;
+		}
+		return true;
+	}
 	
-	public static String getPropertie(STATIONPROP prop){
-		String lreturn ="";
-		switch (prop){
+	public static String getPropertie(STATIONPROP prop) {
+		String lreturn = "";
+
+		lreturn = list.get(prop);
+
+		if (lreturn==null || lreturn.length() == 0) {
+
+			switch (prop) {
 			case DATABASE:
 				lreturn = readProp(database);
 				break;
 			case MASTER:
 				lreturn = readProp(master);
 				break;
+			}
+			list.put(prop, lreturn);
 		}
 		return lreturn;
 	}
 
-	public static  void storePropertie(STATIONPROP prop, String value){
+	public static void storePropertie(STATIONPROP prop, String value) {
 		Properties pro = new Properties();
 		try {
 			pro.load(new FileInputStream(new File(filename)));
@@ -45,18 +67,17 @@ public class Utils {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		switch (prop){
+
+		switch (prop) {
 		case DATABASE:
 			pro.setProperty(database, "localhost");
 			break;
 		case MASTER:
 			pro.setProperty(master, value);
 			break;
-			
-			
+
 		}
-		
+
 		try {
 			pro.store(new FileOutputStream(new File(filename)), "Daten");
 		} catch (FileNotFoundException e) {
@@ -67,9 +88,10 @@ public class Utils {
 			e.printStackTrace();
 		}
 	}
+
 	private static String readProp(String propertie) {
-		
-		if (PropertieFileExist()){
+
+		if (PropertieFileExist()) {
 			Properties prop = new Properties();
 			try {
 				prop.load(new FileInputStream(new File(filename)));
@@ -80,7 +102,7 @@ public class Utils {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-				return prop.getProperty(propertie);
+			return prop.getProperty(propertie);
 		}
 		return "";
 	}
